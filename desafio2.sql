b7ee65f3ae24c8be2d117e5e6af71a28cdd06ee6
BradockBr <robtognetti@gmail.com>
2022-12-05 21:07:36 -0300

    de novo requisitos 2, 3 e 6 tentando passar no avaliador

--- desafio2.sql
+++ desafio2.sql
@@ -2,6 +2,9 @@ SELECT
     COUNT(s.song_id) AS cancoes,
     COUNT(DISTINCT art.artist_id) AS artistas,
     COUNT(DISTINCT a.album_id) AS albuns
-FROM song AS s
-INNER JOIN albums AS a ON s.album_id = a.album_id
-INNER JOIN artists AS ar ON a.artist_id = ar.artist_id;
+FROM
+    songs AS s
+        INNER JOIN
+    albums AS a ON s.album_id = a.album_id
+        INNER JOIN
+    artists AS art ON a.artist_id = art.artist_id;
--- desafio6.sql
+++ desafio6.sql
@@ -3,5 +3,7 @@ SELECT
     MAX(p.plan_price) AS faturamento_maximo,
     ROUND(AVG(p.plan_price), 2) AS faturamento_medio,
     ROUND(SUM(p.plan_price), 2) AS faturamento_total
-FROM plan AS p
-INNER JOIN users AS u ON u.plan_id = p.plan_id ;
+FROM
+    plans AS p
+        INNER JOIN
+    users AS u ON u.plan_id = p.plan_id
